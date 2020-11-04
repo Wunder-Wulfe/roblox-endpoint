bb3a6ca8623c7e27962b9084f8889db07eace130
Wunder Wulfe <doge_is_swagaroony@hotmail.com>
2020-11-04 12:20:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import httpx
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import *
 
 app = FastAPI(
     title = "Roblox Endpoint",
@@ -16,6 +16,31 @@ app = FastAPI(
 
 client = httpx.AsyncClient()
 
-@app.get("/servers/{placeId}", tags = ["Servers"])
-async def find_place(placeId: int):
-    return (await client.get(f"https://games.roblox.com/v1/games/{placeId}/servers/Public")).json()
+async def GET(url: string):
+    return client.get(url)
+async def textGET(url: string):
+    return (await GET(url)).text
+async def jsonGET(url: string):
+    return (await GET(url)).json()
+
+def serverHTML(sdata, cdata)
+    return fr"""<html><head></head><body>
+        <h1 class="header">Should you play {cdata.Name}?</h1>
+        <div id="result"></div>
+        <div id="reason"></div>
+    </body></html>"""
+
+serverErr = r"""
+<html><head></head><body>place does not exist</body></html>
+"""
+
+@app.get("/servers/{placeId}", tags = ["Servers"], response_class = HTMLResponse)
+async def server_data(placeId: int):
+    response = await jsonGET(fr"https://games.roblox.com/v1/games/{placeId}/servers/Public")
+    if "errors" in response:
+        return serverErr
+    else:
+        return serverHTML(
+            response, 
+            await jsonGET(fr"http://api.roblox.com/marketplace/productinfo?assetId={placeId}")
+        )
