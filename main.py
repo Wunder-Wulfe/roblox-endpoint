6d016e6347f83ea5c929f8d089d6e34bcbe428bf
Wunder Wulfe <doge_is_swagaroony@hotmail.com>
2020-11-04 17:40:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,4 @@ app = FastAPI(
 
 @app.get("/servers/{placeId}", tags = ["Servers"])
 async def find_place(placeId: int):
-    return requests.get(
-        f"https://games.roblox.com/v1/games/{placeId}/servers/Public"
-    )
+    return f"https://games.roblox.com/v1/games/{placeId}/servers/Public"
