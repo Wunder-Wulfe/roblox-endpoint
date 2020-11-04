5a8d921d6bff40faafba4dd97b34c3f06b7b8474
Wunder Wulfe <doge_is_swagaroony@hotmail.com>
2020-11-04 07:25:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/servers/{placeId}")
+async def findPlace(placeId: int = 4953508391):
+    return {"test": placeId}
