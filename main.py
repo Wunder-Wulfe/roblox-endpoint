b13e40eacb4560daf1e1477723e72062e4fc2e52
Wunder Wulfe <doge_is_swagaroony@hotmail.com>
2020-11-05 18:09:35 -0600

    updating detection

--- main.py
+++ main.py
@@ -30,22 +30,36 @@ env = Environment(loader=file_loader)
 
 template = env.get_template("serverHTML.html")
 
+def sweatCount(server, sweats: [int]):
+	return sum(player in sweats for player in server['playerIds'])
+
 def getResult(sdata):
 	if len(sdata['data']) == 0:
 		return ('alert', 'No.', 'Nobody is playing this game')
 	else:
 		servers = sdata['data']
-		maxPlayers = max(server['playing'] for server in servers)
+		maxPlayerServer = None
+		maxPlayers = 0
+		for i in range(len(servers)):
+			server = servers[i]
+			if server['playing'] > maxPlayers:
+				maxPlayerServer = server
+				maxPlayers = server['playing']
 		plLimit = max(server['maxPlayers'] for server in servers)
 		if min(server['ping'] for server in servers) > 350:
 			return ('alert', 'No.', 'All servers have awful ping')
 		elif min(server['fps'] for server in servers) < 11:
 			return ('alert', 'No.', 'All servers have awful framerates')
 		elif plLimit > 5:
+			sweatInMax = sweatCount(maxPlayerServer)
 			if maxPlayers == 1:
 				return ('alert', 'No.', 'Theres only one person per server')
 			elif maxPlayers == 2:
 				return ('warn', 'Maybe?', 'Looks like theres 1 v 1s going on')
+			elif sweatInMax == maxPlayers:
+				return ('alert', 'No.', 'Everyone in the biggest server is sweating')
+			elif sweatInMax > 0:
+				return ('warn', 'Maybe?', 'There are some sweats online')
 			elif maxPlayers < 5:
 				return ('warn', 'Maybe?', 'There are only a few people playing per server')
 		return ('success', 'Sure.', 'There are quite a few players')
@@ -67,7 +81,7 @@ serverErr = r"""
 """
 
 @app.get("/servers/{placeId}", tags = ["Servers"], response_class = HTMLResponse)
-async def server_data(placeId: int):
+async def server_data(placeId: int, sweats: [int] = []):
 	response = await jsonGET(fr"https://games.roblox.com/v1/games/{placeId}/servers/Public")
 	if "errors" in response:
 		return serverErr
