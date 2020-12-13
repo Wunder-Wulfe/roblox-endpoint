e60223a58f848e0c6edcf63c6743e749698b94be
Wunder Wulfe <doge_is_swagaroony@hotmail.com>
2020-12-13 16:47:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ env = Environment(loader=file_loader)
 template = env.get_template("serverHTML.html")
 
 def sweatCount(server, sweats: [int]):
-	return sum(player in sweats for player in server['playerIds'])
+	return 0 #sum(player in sweats for player in server['playerIds'])
 
 def rating(voteR):
 	if voteR < 0.31:
